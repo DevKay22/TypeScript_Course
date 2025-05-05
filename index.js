07c8be182ebd3331f0316805cd6052343a8e8506
Kayode <drkay4sure@gmail.com>
2025-05-05 15:41:40 +0100

    calculator project

--- index.js
+++ index.js
@@ -12,4 +12,32 @@ var student1 = {
 function person(student) {
     console.log("Hello everone!!! My name is ".concat(student1.name, ", of school ").concat(student1.school, ". I am ").concat(student1.age, " in ").concat(student1.class, " and my best subject is ").concat(student1.courses[1], ". My courses are ").concat(student1.courses.join(","), " and I have the following scores ").concat(student1.scores.join(","), " in the entrance tests"));
 }
-person(student1);
+// person(student1)
+// Optional parameters
+function greet(name, title) {
+    return title ? "Hello ".concat(title, " ").concat(name, "!") : "Hello ".concat(name, "!");
+}
+// console.log(greet("Dele"));
+// console.log(greet("Dayo", "Prof(Mrs)."));
+// Default parameters
+function hype(name) {
+    if (name === void 0) { name = "Adam"; }
+    return "Hi ".concat(name);
+}
+// console.log(hype());
+// console.log(hype("Tolu"));
+// Calculator project with typed functions 
+var addCalc = function (a, b) {
+    return a + b;
+};
+var subCalc = function (a, b) { return a - b; };
+var multCalc = function (a, b) { return a * b; };
+var divCalc = function (a, b) {
+    return a / b;
+};
+var powerCalc = function (a, n) { return Math.pow(a, n); };
+console.log(addCalc(9, 7));
+console.log(subCalc(76, 7));
+console.log(multCalc(39, 7));
+console.log(divCalc(35, 7));
+console.log(powerCalc(4, 3));
--- index.ts
+++ index.ts
@@ -26,4 +26,47 @@ function person(student: Student) {
     console.log(`Hello everone!!! My name is ${student1.name}, of school ${student1.school}. I am ${student1.age} in ${student1.class} and my best subject is ${student1.courses[1]}. My courses are ${student1.courses.join(",")} and I have the following scores ${student1.scores.join(",")} in the entrance tests`)
 }
 
-person(student1)
+// person(student1)
+
+
+// Optional parameters
+function greet( name: string, title? : string) {
+    return title ? `Hello ${title} ${name}!` : `Hello ${name}!`;
+}
+
+// console.log(greet("Dele"));
+// console.log(greet("Dayo", "Prof(Mrs)."));
+
+
+// Default parameters
+function hype(name: string = "Adam"): string {
+    return `Hi ${name}`;
+}
+
+// console.log(hype());
+// console.log(hype("Tolu"));
+
+
+// Calculator project with typed functions 
+
+const addCalc = (a: number, b: number): number => {
+    return a + b
+}
+
+const subCalc = (a: number, b: number): number =>  a - b
+
+
+const multCalc = (a: number, b: number): number =>  a * b
+
+
+const divCalc = (a: number, b: number): number => {
+    return a / b
+}
+
+const powerCalc = (a: number, n: number): number =>  a ** n
+
+console.log(addCalc(9,7));
+console.log(subCalc(76,7));
+console.log(multCalc(39,7));
+console.log(divCalc(35,7));
+console.log(powerCalc(4,3));
